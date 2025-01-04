7014c3d2ae3a19b0a2e1867d15739fb3f356a5ab
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-04 18:04:55 +0500

    add

---
+++ src/pages/MarketP.jsx
@@ -0,0 +1,11 @@
+import Market from "../components/market/Market"
+
+const MarketP = () => {
+  return (
+    <>
+   <Market/>
+    </>
+  )
+}
+
+export default MarketP
--- src/router/App.jsx
+++ src/router/App.jsx
@@ -4,7 +4,7 @@ import HeaderP from '../pages/Header';
 import Categoriess from '../pages/Categoriess';
 import Employ from '../pages/Employ';
 import Product from '../pages/Product';
-import Shop from '../pages/shop';
+import MarketP from '../pages/MarketP';
 
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/" element={<Login/>} />
         <Route path="/home" element={<HeaderP />} />
         <Route path="/categories" element={<Categoriess />} />
-        <Route path="/shops" element={<Shop />} />
+        <Route path="/shops" element={<MarketP/>} />
         <Route path="/employees" element={<Employ />} />
         <Route path="/ready-product" element={<Product />} /> 
       </Routes>
