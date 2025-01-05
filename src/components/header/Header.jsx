15107d8b86a5a8a3f7efb61ffded8fcdccae4d46
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-05 19:28:05 +0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <div className="header_header">
           <Sidebar />
           <div className="header_box">
-            <button onClick={setOpen}>Toggle Modawxdgit cl</button>
+            <button onClick={setOpen}>Toggle Modawxdgit clwfex</button>
             {open && <Modal />}
           </div>
         </div>
