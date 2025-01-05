dee4a31606ae86c474cc8be512daea7459f4d786
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-06 01:19:41 +0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useStore from "../../zustand"; 
 import Modal from "./Modal";
@@ -7,49 +7,79 @@ import Nav from "../nav/Nav";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [data, setData] = useState(null);
   const { token, refreshToken, setOpen, open } = useStore();
-
+  
   useEffect(() => {
     if (!token) {
       navigate("/");
     } else {
       if (window.location.pathname !== "/home") {
         navigate("/home");
-      }
-      // const interval = setInterval(() => {
-      //   refreshToken(navigate); 
-      // }, 120000);
+         // const interval = setInterval(() => {
+//       //   refreshToken(navigate); 
+//       // }, 120000);
 
-      // return () => clearInterval(interval); 
+//       // return () => clearInterval(interval); 
+      }
     }
-  }, [token, refreshToken, navigate]);
+  }, [token,refreshToken, navigate]);
+
+  useEffect(() => {
+    fetch('https://aqvo.limsa.uz/api/analytics', {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data)) // Directly set the fetched data
+      .catch((error) => {
+        console.error("Error fetching analytics data:", error);
+      });
+  }, []); 
+
+    // fetch('https://aqvo.limsa.uz/api/conserve-type', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Accept': '*/*',
+    //     'Authorization': `Bearer ${token}`,
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((elem) => console.log(elem)) 
+    //   .catch((error) => {
+    //     console.error("Error fetching analytics data:", error);
+    //   });
+
+
   const rows = [
     { name: "Un", quantity: 50 },
     { name: "Shakar", quantity: 30 },
   ];
+  
+
   return (
     <>
       <header>
         <div className="header_header">
           <Sidebar />
           <div className="header_box">
-            <Nav/>
+            <Nav />
             <div className="header_card">
-           <h1>Analitika bolimi</h1>
-           <div className="header_card_child">
-            <ul className="header_wrapper">
-              <li className="header_wrapper_item">ummimiy miqdor</li>
-              <li className="header_wrapper_item">ummumir tushum</li>
-              <li className="header_wrapper_item">ummumiy sotilgan</li>
-            </ul>
-            <div className="header_card_resurse">
-              <h3 className="header_card_resurse_title">Resurs Hisoboti</h3>
-              <p className="header_card_resurse_txt">Mahsulotni tanlang</p>
-              <p className="header_card_resurse_txt">Mahsulotni moqdorini kiriting</p>
-            </div>
-            <div className="header_card_accounting">
-             <h3 className="header_card_accounting_title">Sarflangan resurslar jadvali</h3>
-             <table
+              <h1>Analitika bolimi</h1>
+              <div className="header_card_child">
+                <ul className="header_wrapper">
+
+                  <li className="header_wrapper_item">ummimiy miqdor {data?.totalProduced}</li>
+                  <li className="header_wrapper_item">ummumir tushum {data?.totalProfit}</li>
+                  <li className="header_wrapper_item">ummumiy sotilgan {data?.totalSold}</li>
+                </ul>
+                <div className="header_card_resurse">
+                  <h3 className="header_card_resurse_title">Resurs Hisoboti</h3>
+                  <p className="header_card_resurse_txt">Mahsulotni tanlang</p>
+                  <p className="header_card_resurse_txt">Mahsulotni moqdorini kiriting</p>
+                </div>
+                <div className="header_card_accounting">
+                  <h3 className="header_card_accounting_title">Sarflangan resurslar jadvali</h3>
+                  <table
                     style={{
                       width: "100%",
                       borderCollapse: "collapse",
@@ -72,10 +102,10 @@ const Header = () => {
                       ))}
                     </tbody>
                   </table>
-            </div>
-           </div>
-            <button onClick={setOpen}>Modalni ochish</button>
-            {open && <Modal />}
+                </div>
+              </div>
+              <button onClick={setOpen}>Modalni ochish</button>
+              {open && <Modal />}
             </div>
           </div>
         </div>
