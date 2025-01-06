c51c31204837a5b3d5e29ba921cd0dba95cfb526
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-06 17:05:22 +0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import useStore from "../../zustand"; 
+import useStore from "../../zustand";
 import Modal from "./Modal";
 import Sidebar from "../sidebar/SideBar";
 import Nav from "../nav/Nav";
@@ -8,53 +8,69 @@ import Nav from "../nav/Nav";
 const Header = () => {
   const navigate = useNavigate();
   const [data, setData] = useState(null);
+  const [takeID, setTakeiD] = useState(null)
+  const [dataConserve, setDateConserve] = useState(null);
   const { token, refreshToken, setOpen, open } = useStore();
-  
+
   useEffect(() => {
     if (!token) {
       navigate("/");
     } else {
       if (window.location.pathname !== "/home") {
-        navigate("/home");
-         // const interval = setInterval(() => {
-//       //   refreshToken(navigate); 
-//       // }, 120000);
+        navigate("/home")
+        // const interval = setInterval(() => {
+        //       //   refreshToken(navigate);
+        //       // }, 120000);
 
-//       // return () => clearInterval(interval); 
+        //       // return () => clearInterval(interval);
       }
     }
-  }, [token,refreshToken, navigate]);
+  }, [token, refreshToken, navigate]);
 
   useEffect(() => {
-    fetch('https://aqvo.limsa.uz/api/analytics', {
-      method: 'GET',
+    fetch("https://aqvo.limsa.uz/api/analytics", {
+      method: "GET",
     })
       .then((response) => response.json())
       .then((data) => setData(data)) // Directly set the fetched data
       .catch((error) => {
         console.error("Error fetching analytics data:", error);
       });
-  }, []); 
+  }, []);
 
-    // fetch('https://aqvo.limsa.uz/api/conserve-type', {
-    //   method: 'GET',
-    //   headers: {
-    //     'Accept': '*/*',
-    //     'Authorization': `Bearer ${token}`,
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((elem) => console.log(elem)) 
-    //   .catch((error) => {
-    //     console.error("Error fetching analytics data:", error);
-    //   });
-
+  useEffect(() => {
+    fetch("https://aqvo.limsa.uz/api/conserve-type", {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((elem) => setDateConserve(elem.data))
+      .catch((error) => {
+        console.error("Error fetching analytics data:", error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const rows = [
     { name: "Un", quantity: 50 },
     { name: "Shakar", quantity: 30 },
   ];
-  
+  console.log(dataConserve);
+
+  const select = (event) => {
+    console.log(event.target.value)
+    
+    const selectID = event.target.value;
+    selectID?.map((i)=>(
+      console.log(i)
+    ))
+    setTakeiD(selectID);  
+  };
+  // console.log(takeID);
+
 
   return (
     <>
@@ -67,18 +83,41 @@ const Header = () => {
               <h1>Analitika bolimi</h1>
               <div className="header_card_child">
                 <ul className="header_wrapper">
-
-                  <li className="header_wrapper_item">ummimiy miqdor {data?.totalProduced}</li>
-                  <li className="header_wrapper_item">ummumir tushum {data?.totalProfit}</li>
-                  <li className="header_wrapper_item">ummumiy sotilgan {data?.totalSold}</li>
+                  <li className="header_wrapper_item">
+                    📈
+                    ummimiy miqdor {data?.totalProduced}
+                  </li>
+                  <li className="header_wrapper_item">
+                    🛒
+                    ummumir tushum {data?.totalProfit}
+                  </li>
+                  <li className="header_wrapper_item">
+                    💰
+                    ummumiy sotilgan {data?.totalSold}
+                  </li>
                 </ul>
                 <div className="header_card_resurse">
                   <h3 className="header_card_resurse_title">Resurs Hisoboti</h3>
-                  <p className="header_card_resurse_txt">Mahsulotni tanlang</p>
-                  <p className="header_card_resurse_txt">Mahsulotni moqdorini kiriting</p>
+                  <select name="" id="" onChange={select}>
+                    <option value="" selected disabled>
+                      mahsulotni tanlang
+                    </option>
+                    {dataConserve &&
+                      dataConserve.map((option) => (
+
+                          console.log(option?.productConsumptions)
+                          
+
+                        // <option key={index} value={option.productConsumptions || index}>
+                        //   {option.conserveType || option}
+                        // </option>
+                      ))}
+                  </select>
                 </div>
                 <div className="header_card_accounting">
-                  <h3 className="header_card_accounting_title">Sarflangan resurslar jadvali</h3>
+                  <h3 className="header_card_accounting_title">
+                    Sarflangan resurslar jadvali
+                  </h3>
                   <table
                     style={{
                       width: "100%",
@@ -90,7 +129,9 @@ const Header = () => {
                     <thead>
                       <tr style={{ backgroundColor: "#f4f4f4" }}>
                         <th style={{ padding: "10px" }}>Resurslar nomi</th>
-                        <th style={{ padding: "10px" }}>Resurslar miqdori (kg)</th>
+                        <th style={{ padding: "10px" }}>
+                          Resurslar miqdori (kg)
+                        </th>
                       </tr>
                     </thead>
                     <tbody>
